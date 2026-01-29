99ed96e919040b1f8a4d60cd4caaedc1b1a93112
Isha Agrawal <isha.agrawal1611@gmail.com>
2026-01-29 18:09:11 +0530

    Time: 3291 ms (5.05%), Space: 20 MB (34.08%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.py
@@ -0,0 +1,31 @@
+class Foo:
+    def __init__(self):
+        pass
+        self.num = 0
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.num += 1
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        
+        # printSecond() outputs "second". Do not change or remove this line.
+        while True:
+            if self.num == 1:
+                printSecond()
+                self.num += 1
+                break
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        
+        # printThird() outputs "third". Do not change or remove this line.
+        while True:
+            if self.num == 2:
+                printThird()
+                self.num += 1
+                break
