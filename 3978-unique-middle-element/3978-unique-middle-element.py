1189edcdb9c233090b107e5456b902ad3a532ff5
Isha Agrawal <isha.agrawal1611@gmail.com>
2026-07-06 18:25:20 +0530

    Time: 0 ms (100%), Space: 19.3 MB (66.67%) - LeetHub

--- 3978-unique-middle-element/3978-unique-middle-element.py
+++ 3978-unique-middle-element/3978-unique-middle-element.py
@@ -1,10 +1,4 @@
-from collections import Counter
 class Solution:
     def isMiddleElementUnique(self, nums: list[int]) -> bool:
-        n = len(nums)//2
-        middle = nums[n]
-        count = Counter(nums)
-        if  count[middle] == 1:
-            return True
-        return False
+        return nums.count(nums[len(nums)//2]) == 1
         
